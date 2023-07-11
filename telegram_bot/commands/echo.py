87670e6b7541df84039647e4ca7f49162a2d6c04
Anton Skopin <askopin@gmail.com>
2023-07-11 18:33:45 +0200

    Moved token to .env file + configured VSCode environment

--- telegram_bot/commands/echo.py
+++ telegram_bot/commands/echo.py
@@ -1,8 +1,15 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from telegram.constants import MessageEntityType
+from ..helpers.string_helpers import StringRange, clear_ranges
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    command_ranges = [StringRange(e.offset, e.length) for e 
+                        in update.message.entities
+                        if e.type == MessageEntityType.BOT_COMMAND]
+    reply_text = clear_ranges(update.message.text, command_ranges)
+
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=update.message.text
+        text=reply_text
     )
